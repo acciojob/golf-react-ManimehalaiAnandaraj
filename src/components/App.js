707292775a2766c01a69787e649f7f062212882c
manimehalaianandaraj <manimehalaiannakodi@gmail.com>
2025-05-02 13:29:36 +0530

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,46 @@
-import React, { Component, useState } from "react";
+import React, { Component } from "react";
 import '../styles/App.css';
 
 class App extends Component {
     constructor(props) {
-        super(props)
+        super(props);
         this.state = {
             renderBall: false,
-            posi : 0,
+            posi: 0,
             ballPosition: { left: "0px" }
         };
-        this.renderChoice = this.renderBallOrButton.bind(this)
-        this.buttonClickHandler = this.buttonClickHandler.bind(this)
+        this.renderChoice = this.renderBallOrButton.bind(this);
+        this.buttonClickHandler = this.buttonClickHandler.bind(this);
+        this.handleKeyDown = this.handleKeyDown.bind(this);
     };
 
     buttonClickHandler() {
-   
-   }
-    renderBallOrButton() {
-		if (this.state.renderBall) {
-		    return <div className="ball" style={this.state.ballPosition}></div>
-		} else {
-		    return <button onClick={this.buttonClickHandler} >Start</button>
-		}
+        this.setState({ renderBall: true });
+        document.addEventListener("keydown", this.handleKeyDown);
     }
 
-    // bind ArrowRight keydown event
-    componentDidMount() {
-      
+    handleKeyDown(event) {
+        if (event.key === "ArrowRight") {
+            this.setState(prevState => {
+                const newPos = prevState.posi + 10; // Move 10px to the right
+                return {
+                    posi: newPos,
+                    ballPosition: { left: `${newPos}px` }
+                };
+            });
+        }
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener("keydown", this.handleKeyDown);
+    }
+
+    renderBallOrButton() {
+        if (this.state.renderBall) {
+            return <div className="ball" style={this.state.ballPosition}></div>
+        } else {
+            return <button onClick={this.buttonClickHandler}>Start</button>
+        }
     }
 
     render() {
@@ -37,6 +51,5 @@ class App extends Component {
         )
     }
 }
-
 
 export default App;
