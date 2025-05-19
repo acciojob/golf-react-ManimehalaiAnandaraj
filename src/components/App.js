40baf3d1593c7da7fb3aaf16d8daae9fdb275665
manimehalaianandaraj <manimehalaiannakodi@gmail.com>
2025-05-19 14:04:23 +0530

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import './../styles/App.css';
 
-class App extends React.Component
- {
+class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       isGameStarted: false,
-      ballPosition: 0
+      ballPosition: 0,
+      gameWidth: window.innerWidth // Get the width of the window for boundary checking
     };
     this.handleStart = this.handleStart.bind(this);
     this.handleKeyPress = this.handleKeyPress.bind(this);
@@ -21,20 +22,28 @@ class App extends React.Component
   }
 
   handleStart() {
-    this.setState({ isGameStarted: true });
+    this.setState({ 
+      isGameStarted: true,
+      ballPosition: 0
+    });
   }
 
   handleKeyPress(event) {
-    if (this.state.isGameStarted && event.keyCode === 39) {
-      this.setState(prevState => ({
-        ballPosition: prevState.ballPosition + 5
-      }));
+    if (this.state.isGameStarted && event.keyCode === 39) { // Right arrow key
+      this.setState(prevState => {
+        const newPosition = prevState.ballPosition + 5;
+        // Check if the new position is within the game width
+        if (newPosition <= prevState.gameWidth - 50) { // Assuming the ball width is 50px
+          return { ballPosition: newPosition };
+        }
+        return prevState; // Don't update if the ball goes out of bounds
+      });
     }
   }
 
   render() {
     return (
-      <div>
+      <div className='golf-game'>
         {!this.state.isGameStarted && (
           <button className="start" onClick={this.handleStart}>
             Start Game
@@ -43,7 +52,7 @@ class App extends React.Component
         {this.state.isGameStarted && (
           <div
             className="ball"
-            style={{ left: `${this.state.ballPosition}px` }}
+            style={{ left: `${this.state.ballPosition}px`, position: 'absolute' }} // Ensure position is absolute
           />
         )}
       </div>
